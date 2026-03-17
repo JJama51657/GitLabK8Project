04b3be188c1aa74ae973adf561492be2b7737cce
Jama <jjama51657@gmail.com>
2026-03-17 20:11:50 +0000

    10.11

--- TerraformEKS/helm-provider.tf
+++ TerraformEKS/helm-provider.tf
@@ -1,21 +1,31 @@
 data "aws_eks_cluster" "helm_cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 data "aws_eks_cluster_auth" "helm_cluster" {
-  name = module.eks.cluster_name
+  name       = module.eks.cluster_name
+  depends_on = [module.eks]
 }
 
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.helm_cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.helm_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.helm_cluster.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+  }
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.helm_cluster.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.helm_cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.helm_cluster.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.region]
+    }
   }
 }
